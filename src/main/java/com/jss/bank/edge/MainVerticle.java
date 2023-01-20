b6db5f167605ae1cd64ba495f60235e904230d7c
Jonatan Silva <jonatan.tab58@gmail.com>
2023-01-19 21:41:44 -0300

    ✅ Restructuring deployment base

--- src/main/java/com/jss/bank/edge/MainVerticle.java
+++ src/main/java/com/jss/bank/edge/MainVerticle.java
@@ -2,6 +2,8 @@ package com.jss.bank.edge;
 
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.core.http.HttpServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,9 +15,21 @@ public class MainVerticle extends AbstractVerticle {
   @Override
   public Uni<Void> asyncStart() {
     final Uni<HttpServer> server = vertx.createHttpServer()
+        .requestHandler(handler -> {
+          handler.response().end("base").subscribe();
+        })
         .listen(8080)
         .onItem().invoke(() -> logger.info("HTTP server started on port 8080"));
 
     return server.replaceWithVoid();
+  }
+
+  public static void main(String[] args) {
+    final Vertx vertx = Vertx.vertx();
+
+    vertx.deployVerticle(MainVerticle::new, new DeploymentOptions())
+        .subscribe()
+        .with(
+            ok -> logger.info("Main Verticle has been deployed successfully"), err -> logger.error("Fail on deployment", err));
   }
 }
