e898170791081a3195de69c2b9b159367c6296c7
Jonatan Silva <jonatan.tab58@gmail.com>
2023-01-19 22:24:02 -0300

    ✨ Database configuration

--- src/main/java/com/jss/bank/edge/MainVerticle.java
+++ src/main/java/com/jss/bank/edge/MainVerticle.java
@@ -5,8 +5,11 @@ import io.smallrye.mutiny.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.core.http.HttpServer;
+import org.hibernate.reactive.mutiny.Mutiny;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.persistence.Persistence;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -14,6 +17,17 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public Uni<Void> asyncStart() {
+    Uni<Void> hibernate = Uni.createFrom().deferred(() -> {
+      Persistence
+          .createEntityManagerFactory("mysql-edge-bank")
+          .unwrap(Mutiny.SessionFactory.class);
+
+      return Uni.createFrom().voidItem();
+    });
+
+    hibernate = vertx.executeBlocking(hibernate)
+        .onItem().invoke(() -> logger.info("Hibernate Reactive is ready"));
+
     final Uni<HttpServer> server = vertx.createHttpServer()
         .requestHandler(handler -> {
           handler.response().end("base").subscribe();
@@ -21,7 +35,7 @@ public class MainVerticle extends AbstractVerticle {
         .listen(8080)
         .onItem().invoke(() -> logger.info("HTTP server started on port 8080"));
 
-    return Uni.combine().all().unis(server).discardItems();
+    return Uni.combine().all().unis(server, hibernate).discardItems();
   }
 
   public static void main(String[] args) {
